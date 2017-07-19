fa6eb9294a8af703f564841e8290cd9c90ec01a1
Feedtank Account <info@feedtank.com>
2017-07-19 17:33:52 -0400

    added stats.js dependency npm

--- src/components/ThreeJS.vue
+++ src/components/ThreeJS.vue
@@ -5,48 +5,91 @@
 <script>
 
 import * as THREE from 'three'
+import Stats from 'stats.js'
+import OrbitControls from 'three-orbitcontrols'
+
+
+var statsMem;
+
 
 export default {
   
   name: 'ThreeJS',
 
-  mounted: function (){
-    
-    this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
-    
-    this.camera.position.z = 400;
+  created: function(){
 
-    var texture = new THREE.TextureLoader().load( 'static/textures/logo.png' );
-    var geometry = new THREE.BoxBufferGeometry( 100, 100, 100 );
-    var material = new THREE.MeshBasicMaterial( { map: texture } );
+
+    this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, .1, 1000);
+    this.camera.up = new THREE.Vector3(0,1,0);
+    this.camera.position.set(0,0,-1);
+    this.camera.lookAt(new THREE.Vector3(0,0,0));
     
+    var texture = new THREE.TextureLoader().load( 'static/textures/crate.gif' );
+    var geometry = new THREE.BoxBufferGeometry( .4, .4, .4);
+    var material = new THREE.MeshBasicMaterial( { map: texture } );
     this.mesh = new THREE.Mesh( geometry, material );
+    this.mesh.position.set(0,0,0);
+    console.log(this.mesh.position);
+
     this.scene.add( this.mesh );
     this.renderer.setPixelRatio( window.devicePixelRatio );
     this.renderer.setClearColor(0xFFFFFF);
     this.renderer.setSize( window.innerWidth, window.innerHeight );
+   
+    this.statsFPS = new Stats();
+
+    
+
+  },
+  mounted: function (){
+    
     this.$refs.eleCanvasContainer.appendChild( this.renderer.domElement );
 
-    this.animate();
+    this.statsFPS.showPanel( 0 );
+
+    document.body.appendChild( this.statsFPS.domElement );
+
+    this.controls = new OrbitControls(this.camera,this.$refs.eleCanvasContainer);
+    this.controls.rotateSpeed = .5;
+    this.keyPanSpeed = 0.01; 
+    this.controls.enableDamping = true;
+    this.controls.dampingFactor = 0.25;
+    this.controls.enableZoom = true;
+    this.controls.target.set(0,0,0);
+    
+
+
+    requestAnimationFrame( this.renderGL );
+
 
   },
   methods: {
-    animate:function(){
+    renderGL:function(){
+      
+      this.controls.update();
+      
+      
+      // this.stats.update()
+      // this.mesh.rotation.x += 0.02;
+      // this.mesh.rotation.y += 0.08;
 
-      requestAnimationFrame( this.animate );
+      // this.mesh.position =  new THREE.Vector3(0,0,0);
 
-      this.mesh.rotation.x += 0.04;
-      this.mesh.rotation.y += 0.08;
       this.renderer.render( this.scene, this.camera );
+      this.statsFPS.update();
 
+      requestAnimationFrame( this.renderGL );
+      
     }
   },
   data () {
     return {
-      camera: null,
       scene: new THREE.Scene(),
       renderer: new THREE.WebGLRenderer(),
-      mesh:  null
+      camera: null,
+      mesh: null,
+      controls: null,
+      statsFPS: new Stats()
     }
   }
 }
